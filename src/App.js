5a956f980a00c6b282d52be55577fd5dc00636bc
md-sarower <sarower9415@gmail.com>
2022-05-09 14:30:33 +0600

    before using ready made hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
+import Products from './components/products/Products';
 import Register from './components/register/Register';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </div>
